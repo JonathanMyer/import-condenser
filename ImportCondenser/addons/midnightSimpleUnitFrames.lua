a7609f310058f85a91c1e9b75837bcc2081196eb
Jon Myer <ryepizor2174@gmail.com>
2026-01-20 21:03:53 -0500

    Add support for MidnightSimpoleUnitFrames.

---
+++ ImportCondenser/addons/midnightSimpleUnitFrames.lua
@@ -0,0 +1,24 @@
+local ADDON_NAME, ns = ...
+local ImportCondenser = ns.Addon
+
+ImportCondenser.MidnightSimpleUnitFrames = {}
+
+function ImportCondenser.MidnightSimpleUnitFrames:Import(importString, profileName)
+    if importString and profileName and
+        type(_G.MSUF_Profiles_ImportFromString) == "function" and
+        type(_G.MSUF_CreateProfile) == "function" and
+        type(_G.MSUF_SwitchProfile) == "function"
+    then
+        _G.MSUF_CreateProfile(profileName)
+        _G.MSUF_SwitchProfile(profileName)
+        _G.MSUF_Profiles_ImportFromString(importString)
+    end
+end
+
+
+function ImportCondenser.MidnightSimpleUnitFrames:Export(table)
+    if type(_G.MSUF_Profiles_ExportSelectionToString) == "function" then
+        local profile = _G.MSUF_Profiles_ExportSelectionToString("all")
+        table["MidnightSimpleUnitFrames"] = profile
+    end
+end
