618b8c3cf78f0e14c35ccf0a6066778997b9b360
Jon Myer <ryepizor2174@gmail.com>
2026-01-24 22:59:35 -0500

    Add ability to use Twintop's import/export functions. Added a dropdown type option for export options.

--- ImportCondenser/addons/twintopInsanityBar.lua
+++ ImportCondenser/addons/twintopInsanityBar.lua
@@ -4,19 +4,39 @@ local ImportCondenser = ns.Addon
 ImportCondenser.TwintopInsanityBar = {}
 -- ImportCondenser.db.global.TwintopInsanityBar = {}
 
-local AllOptions = {
-    "Core"
+local EXPORT_STRING_PREFIX = "!TRB"
+
+local classes = {
+    [0] = "All Classes"
 }
 for i, className in ipairs(ImportCondenser.ClassNames) do
-    table.insert(AllOptions, className)
+    table.insert(classes, className)
 end
 
 function ImportCondenser.TwintopInsanityBar:GetExportOptions()
-    return AllOptions, {[1] = UnitClass("player")}, true
+    local args = {
+        [1] = {name = "Classes", values = classes},
+        [2] = "Core",
+        [3] = "Bar Display",
+        [4] = "Thresholds",
+        [5] = "Font and Text",
+        [6] = "Audio and Tracking",
+        [7] = "Bar Text",
+    }
+    return args, {[1] = UnitClass("player")}, false
 end
 
 
 function ImportCondenser.TwintopInsanityBar:DetectIssues(importString)
+    -- cant have issues if its a twintop generated string
+    if EXPORT_STRING_PREFIX == importString:sub(1, #EXPORT_STRING_PREFIX) and
+        _G.Twintop_API and
+        _G.Twintop_API.ImportConfiguration and
+        type(_G.Twintop_API.ImportConfiguration) == "function"
+    then
+        return nil
+    end
+
    if _G.Twintop_Data and _G.Twintop_Data.settings then
 
         local asTable = ImportCondenser:DeSeriPressCode(importString)
@@ -29,6 +49,15 @@ function ImportCondenser.TwintopInsanityBar:DetectIssues(importString)
 end
 
 function ImportCondenser.TwintopInsanityBar:Import(importString)
+    if EXPORT_STRING_PREFIX == importString:sub(1, #EXPORT_STRING_PREFIX) and
+        _G.Twintop_API and
+        _G.Twintop_API.ImportConfiguration and
+        type(_G.Twintop_API.ImportConfiguration) == "function"
+    then
+        _G.Twintop_API.ImportConfiguration(importString)
+        return
+    end
+
     if _G.Twintop_Data and _G.Twintop_Data.settings then
         local asTable = ImportCondenser:DeSeriPressCode(importString)
         for k, v in pairs(asTable) do
@@ -40,13 +69,21 @@ function ImportCondenser.TwintopInsanityBar:Import(importString)
 end
 
 function ImportCondenser.TwintopInsanityBar:Export(table)
-    if _G.Twintop_Data and _G.Twintop_Data.settings then
-        local exportTable = {}
-        for k, v in pairs(_G.Twintop_Data.settings) do
-            if ImportCondenser.db.global.TwintopInsanityBar.selectedExportOptions[k] == true then
-                exportTable[k] = v
-            end
-        end
-        table["TwintopInsanityBar"] = ImportCondenser:SeriPressCode(exportTable)
+    if 
+        _G.Twintop_API and
+        _G.Twintop_API.ExportConfiguration and
+        type(_G.Twintop_API.ExportConfiguration) == "function" and
+        ImportCondenser.db.global.TwintopInsanityBar.selectedExportOptions
+     then
+        local exportOptions = ImportCondenser.db.global.TwintopInsanityBar.selectedExportOptions
+        local barDisplay = exportOptions["Bar Display"] or false
+        local thresholds = exportOptions["Thresholds"] or false
+        local fontAndText = exportOptions["Font and Text"] or false    
+        local audioAndTracking = exportOptions["Audio and Tracking"] or false
+        local barText = exportOptions["Bar Text"] or false
+        local class = exportOptions["Classes"] ~= 0 and exportOptions["Classes"] or nil
+        local core = exportOptions["Core"] or false
+        local newExport = _G.Twintop_API.ExportConfiguration(class, nil, barDisplay, thresholds, fontAndText, audioAndTracking, barText, core)
+        table["TwintopInsanityBar"] = newExport
     end
 end
