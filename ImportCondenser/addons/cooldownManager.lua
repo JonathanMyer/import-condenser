c927902efb1afd5020c5d3634090772da3572b51
Jon Myer <ryepizor2174@gmail.com>
2026-01-29 12:21:29 -0500

    remove cooldown manager check for import option since shouldImport handles that now

--- ImportCondenser/addons/cooldownManager.lua
+++ ImportCondenser/addons/cooldownManager.lua
@@ -4,7 +4,7 @@ local ImportCondenser = ns.Addon
 ImportCondenser.CooldownManager = {}
 
 function ImportCondenser.CooldownManager:GetExportOptions()
-    return {"Export"}, {}, false
+    return { "Export" }, {}, false
 end
 
 function ImportCondenser.CooldownManager:DetectIssues(importString)
@@ -17,9 +17,7 @@ function ImportCondenser.CooldownManager:DetectIssues(importString)
 end
 
 function ImportCondenser.CooldownManager:Import(importString, profileName)
-    if ImportCondenser.db.global.CooldownManager.selectedImportOptions["import"] then
-        C_CooldownViewer.SetLayoutData(importString, profileName)
-    end
+    C_CooldownViewer.SetLayoutData(importString, profileName)
 end
 
 function ImportCondenser.CooldownManager:Export(exports)
