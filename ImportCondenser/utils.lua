db4a851a8f2b875f751ad44d4111ad72de842201
Jon Myer <ryepizor2174@gmail.com>
2026-02-16 07:45:55 -0500

    Add util for import/export ace addons

--- ImportCondenser/utils.lua
+++ ImportCondenser/utils.lua
@@ -1,5 +1,9 @@
 local ADDON_NAME, ns = ...
-ns = ns or {}
+ns                   = ns or {}
+local AceAddon       = LibStub("AceAddon-3.0", true)
+local AceDBOptions   = LibStub("AceDBOptions-3.0", true)
+local LibDualSpec    = LibStub("LibDualSpec-1.0", true)
+
 
 local ImportCondenser = ns.Addon
 if not ImportCondenser then
@@ -84,4 +88,73 @@ function ImportCondenser:ReplaceDelimitedTokenValue(inputStr, token, newValue)
     end
 
     return string.sub(inputStr, 1, lastEnd) .. newValue
+end
+
+function ImportCondenser:ExportAceAddon(addonName)
+    local addon = AceAddon and AceAddon:GetAddon(addonName, true)
+    if not addon then
+        return
+    end
+    local profileOptions
+    if AceDBOptions and addon.db then
+        profileOptions = AceDBOptions:GetOptionsTable(addon.db)
+        -- Enhance profile options with LibDualSpec if available
+        if LibDualSpec then
+            LibDualSpec:EnhanceOptions(profileOptions, addon.db)
+        end
+    end
+
+
+    local handler = profileOptions.handler
+    local profile = handler.db.profile
+
+    if profile then
+        -- Build export structure with main profile and child profiles
+        local exportProfile = {
+            profile = profile,
+            children = {}
+        }
+
+        for childName, childDb in pairs(handler.db.children) do
+            exportProfile.children[childName] = {}
+            ImportCondenser:CopyTable(childDb.profile, exportProfile.children[childName])
+        end
+
+
+        return ImportCondenser:SeriPressCode(exportProfile)
+    end
+end
+
+function ImportCondenser:ImportAceAddon(addonName, importStr, profileName)
+    print("Importing profile for addon: " .. addonName)
+    local addon = AceAddon and AceAddon:GetAddon(addonName, true)
+    if not addon then
+        return
+    end
+
+    local profileOptions
+    if AceDBOptions and addon.db then
+        profileOptions = AceDBOptions:GetOptionsTable(addon.db)
+        -- Enhance profile options with LibDualSpec if available
+        if LibDualSpec then
+            LibDualSpec:EnhanceOptions(profileOptions, addon.db)
+        end
+    end
+
+    local handler = profileOptions.handler
+    handler.db.SetProfile(handler.db, profileName)
+
+    if addon then
+        local import = ImportCondenser:DeSeriPressCode(importStr)
+        ImportCondenser:AddToInspector(import, addonName .. " Import Data")
+
+        -- Import main profile
+        handler.db.profiles[profileName] = import.profile
+
+        for childName, childProfile in pairs(import.children or {}) do
+            if handler.db.children[childName] then
+                ImportCondenser:CopyTable(childProfile, handler.db.children[childName].profile)
+            end
+        end
+    end
 end
